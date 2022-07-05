35830b10ea2ba9c1ecb8194fb455fad510702438
Thang Vu <ducthang310@gmail.com>
2022-07-05 11:14:46 +0700

    Remove subFamily

--- src/modules/sub-family/dto/update-sub-family.dto.ts
+++
@@ -1,4 +0,0 @@
-import { PartialType } from '@nestjs/swagger';
-import { CreateSubFamilyDto } from './create-sub-family.dto';
-
-export class UpdateSubFamilyDto extends PartialType(CreateSubFamilyDto) {}
